5de0af258bd3550c4b3f650f78a627946a21344c
rdandall <robert@notebrush.com>
2017-09-30 18:05:20 -0400

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+public class Example {
+    private long id;
+    private String name;
+    private String artist;
+    private String album;
+    private String rating;
+    private String year;
+
+    public Example(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
